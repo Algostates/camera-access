d2de6e44a5ed88fa8a13b74372304d4eb15fe326
habib <74493137+Algostates@users.noreply.github.com>
2021-01-16 17:28:59 +0530

    new files added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function Main(){
+    let lVid = document.getElementById('localvideo');
+    document.getElementById('access--btn').addEventListener('click', _ => {
+        _.preventDefault();
+        _.stopPropagation();
+        let medConstraints = {
+            video: true,
+            audio: true
+        };
+        let ret = navigator.mediaDevices.getUserMedia(medConstraints);
+        console.log();
+        ret.then(stream => {
+            console.log('Got Stream:', stream);
+        }).catch(err => {
+            console.log('Couldn\'t get access to the camera/mic:', err);
+        }); 
+    }, !1);   
+    
+}
+
+document.addEventListener('readystatechange', _ => document.readyState === 'complete' && Main(), !1);
